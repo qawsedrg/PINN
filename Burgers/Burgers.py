514a0556922c856774beee21979d9bb7e3d6f9b5
CHZ <qawsedrg@126.com>
2023-01-07 13:08:34 +0100

    Burgers equation

--- Burgers/Burgers.py
+++ Burgers/Burgers.py
@@ -1,3 +1,4 @@
+import matplotlib.gridspec as gridspec
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.io
@@ -65,7 +66,7 @@ class PINN(nn.Module):
 data = scipy.io.loadmat('burgers_shock.mat')
 t = data['t'].flatten()[:, None]
 x = data['x'].flatten()[:, None]
-Exact = (np.real(data['usol']).T)
+Exact_ori = np.real(data['usol']).T
 
 X, T = np.meshgrid(x, t)
 grid = np.hstack((X.flatten()[:, None], T.flatten()[:, None]))
@@ -77,7 +78,7 @@ u_bc2 = np.zeros_like(t)
 grid_bc3 = np.hstack((-np.ones_like(t), t))  # (-1,t)
 u_bc3 = np.zeros_like(t)
 
-Exact = Exact.flatten()[:, None]
+Exact = Exact_ori.flatten()[:, None]
 
 grid_bc = np.vstack([grid_bc1, grid_bc2, grid_bc3])
 u_bc = np.vstack([u_bc1, u_bc2, u_bc3])
@@ -95,7 +96,8 @@ optimizer = torch.optim.Adam(dnn.parameters(), lr=1e-3)
 criterion = torch.nn.MSELoss()
 
 plt.ion()
-fig = plt.figure(figsize=(9, 5))
+fig1 = plt.figure(figsize=(9, 5))
+fig2 = plt.figure(figsize=(14, 10))
 
 with tqdm(range(5000)) as bar:
     for epoch in bar:
@@ -123,17 +125,17 @@ with tqdm(range(5000)) as bar:
 
             u = griddata(grid, u.flatten(), (X, T), method='cubic')
 
-            ax = fig.add_subplot(111)
+            ax1 = fig1.add_subplot(111)
 
-            h = ax.imshow(u.T, interpolation='nearest', cmap='rainbow',
-                          extent=[t.min(), t.max(), x.min(), x.max()],
-                          origin='lower', aspect='auto')
-            divider = make_axes_locatable(ax)
+            h = ax1.imshow(u.T, interpolation='nearest', cmap='rainbow',
+                           extent=[t.min(), t.max(), x.min(), x.max()],
+                           origin='lower', aspect='auto')
+            divider = make_axes_locatable(ax1)
             cax = divider.append_axes("right", size="5%", pad=0.10)
-            cbar = fig.colorbar(h, cax=cax)
+            cbar = fig1.colorbar(h, cax=cax)
             cbar.ax.tick_params(labelsize=15)
 
-            ax.plot(
+            ax1.plot(
                 grid_bc[:, 1],
                 grid_bc[:, 0],
                 'kx', label='Data (%d points)' % (u_bc.shape[0]),
@@ -143,22 +145,78 @@ with tqdm(range(5000)) as bar:
             )
 
             line = np.linspace(x.min(), x.max(), 2)[:, None]
-            ax.plot(t[25] * np.ones((2, 1)), line, 'w-', linewidth=1)
-            ax.plot(t[50] * np.ones((2, 1)), line, 'w-', linewidth=1)
-            ax.plot(t[75] * np.ones((2, 1)), line, 'w-', linewidth=1)
+            ax1.plot(t[25] * np.ones((2, 1)), line, 'w-', linewidth=1)
+            ax1.plot(t[50] * np.ones((2, 1)), line, 'w-', linewidth=1)
+            ax1.plot(t[75] * np.ones((2, 1)), line, 'w-', linewidth=1)
 
-            ax.set_xlabel('$t$', size=20)
-            ax.set_ylabel('$x$', size=20)
-            ax.legend(
+            ax1.set_xlabel('$t$', size=20)
+            ax1.set_ylabel('$x$', size=20)
+            ax1.legend(
                 loc='upper center',
                 bbox_to_anchor=(0.9, -0.05),
                 ncol=5,
                 frameon=False,
                 prop={'size': 15}
             )
-            ax.set_title('$u(t,x)$', fontsize=20)
-            ax.tick_params(labelsize=15)
+            ax1.set_title('$u(t,x)$', fontsize=20)
+            ax1.tick_params(labelsize=15)
+
+            ax2 = fig2.add_subplot(111)
+
+            gs1 = gridspec.GridSpec(1, 3)
+            gs1.update(top=1 - 1.0 / 3.0 - 0.1, bottom=1.0 - 2.0 / 3.0, left=0.1, right=0.9, wspace=0.5)
+
+            ax2 = plt.subplot(gs1[0, 0])
+            ax2.plot(x, Exact_ori[25, :], 'b-', linewidth=2, label='Exact')
+            ax2.plot(x, u[25, :], 'r--', linewidth=2, label='Prediction')
+            ax2.set_xlabel('$x$')
+            ax2.set_ylabel('$u(t,x)$')
+            ax2.set_title('$t = 0.25$', fontsize=15)
+            ax2.axis('square')
+            ax2.set_xlim([-1.1, 1.1])
+            ax2.set_ylim([-1.1, 1.1])
+
+            for item in ([ax2.title, ax2.xaxis.label, ax2.yaxis.label] +
+                         ax2.get_xticklabels() + ax2.get_yticklabels()):
+                item.set_fontsize(15)
+
+            ax2 = plt.subplot(gs1[0, 1])
+            ax2.plot(x, Exact_ori[50, :], 'b-', linewidth=2, label='Exact')
+            ax2.plot(x, u[50, :], 'r--', linewidth=2, label='Prediction')
+            ax2.set_xlabel('$x$')
+            ax2.set_ylabel('$u(t,x)$')
+            ax2.axis('square')
+            ax2.set_xlim([-1.1, 1.1])
+            ax2.set_ylim([-1.1, 1.1])
+            ax2.set_title('$t = 0.50$', fontsize=15)
+            ax2.legend(
+                loc='upper center',
+                bbox_to_anchor=(0.5, -0.15),
+                ncol=5,
+                frameon=False,
+                prop={'size': 15}
+            )
+
+            for item in ([ax2.title, ax2.xaxis.label, ax2.yaxis.label] +
+                         ax2.get_xticklabels() + ax2.get_yticklabels()):
+                item.set_fontsize(15)
+
+            ax2 = plt.subplot(gs1[0, 2])
+            ax2.plot(x, Exact_ori[75, :], 'b-', linewidth=2, label='Exact')
+            ax2.plot(x, u[75, :], 'r--', linewidth=2, label='Prediction')
+            ax2.set_xlabel('$x$')
+            ax2.set_ylabel('$u(t,x)$')
+            ax2.axis('square')
+            ax2.set_xlim([-1.1, 1.1])
+            ax2.set_ylim([-1.1, 1.1])
+            ax2.set_title('$t = 0.75$', fontsize=15)
+
+            for item in ([ax2.title, ax2.xaxis.label, ax2.yaxis.label] +
+                         ax2.get_xticklabels() + ax2.get_yticklabels()):
+                item.set_fontsize(15)
+
             plt.pause(0.1)
-            fig.clf()
+            fig2.clf()
+            fig1.clf()
 
 plt.ioff()
